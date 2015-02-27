776d87d6cf60071594ed4306a11ecdbd7915a164
miglrodri <migl.rodri@gmail.com>
2015-02-27 23:41:58 +0000

    added pages controller

---
+++ application/controllers/pages.php
@@ -0,0 +1,10 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Pages extends CI_Controller {
+
+	public function index()
+	{
+		//$this->load->view('welcome_message');
+        echo "Hello, world!";
+	}
+}
